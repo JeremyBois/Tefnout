e84556eff0cee04bcd4b9b1fdd8bb2afa14b027f
JeremyBois <JeremyBoisGit@pm.me>
2021-01-20 17:14:38 +0100

    ✨ Add default project structure with cmake configurations for engine (Lib) and samples (Exe).

---
+++ Samples/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "Tefranout.hpp"
+
+int main()
+{
+    Tefranout::GameObject obj;
+    std::cout << obj.GetID() << std::endl;
+}
---
+++ Tefranout/src/Tefranout.hpp
@@ -0,0 +1,7 @@
+#ifndef __TEFRANOUT__HPP
+#define __TEFRANOUT__HPP
+
+// @ TEST
+#include "GameObject.hpp"
+
+#endif
