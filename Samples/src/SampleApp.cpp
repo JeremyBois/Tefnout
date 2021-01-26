96f9b76e1badd1f7f84067105f974ba551c2d3d0
JeremyBois <JeremyBoisGit@pm.me>
2021-01-26 21:28:00 +0100

    🐛 ♻️ - Correct implementation (fix init, fix special case when buffer empty, fix forward iteration, fix required type to please STL). Move Iteration implementation to end of buffer class (`Ring`). Constant and Reverse still not working.

--- Samples/src/SampleApp.cpp
+++ Samples/src/SampleApp.cpp
@@ -14,29 +14,46 @@ Tefnout::Application* Tefnout::CreateApplication()
 {
     // Test ring buffer
     auto buffer = Buffer::Ring<int, 5> {};
-    buffer.Push(0);
-    // buffer.Push(1);
-    // buffer.Push(2);
-    // buffer.Push(3);
-    // buffer.Push(4);
-    // Should overflow
-    // buffer.Push(5);
 
+    cout << "Add until size limit" << endl;
+    buffer.Push(0);
+    buffer.Push(1);
+    buffer.Push(2);
+    buffer.Push(3);
+    buffer.Push(4);
+    // Should overflow
+    cout << "Add more than capacity --> should Overflow" << endl;
+    cout << (buffer.TryPush(5) == Buffer::Response::Overflow) << endl;
+
+    cout << "Test front / back" << endl;
     cout << buffer.Front() << endl;
     cout << buffer.Back() << endl;
-    // cout << buffer.Pop() << endl;
     cout << endl;
 
-    cout << (*buffer.begin()) << endl;
-    cout << (*buffer.end()) << endl;
-    cout << ((*buffer.begin()) == (*buffer.end())) << endl;
+    cout << "Loop over (non-const) from 0 to 4" << endl;
+    for (auto i : buffer)
+    {
+        cout << "for: " << i << "\n";
+    }
     cout << endl;
+
+    // cout << "Loop over (const with iterator) from 0 to 4" << endl;
+    // for (auto it = buffer.cbegin(); it != buffer.cend(); it++)
+    // {
+    //     std::cout << *it << '\n';
+    // }
+    // cout << endl;
+
+    // cout << "Loop in reverse order 4 to 0" << endl;
+    // for (auto it = buffer.rbegin(); it != buffer.rend(); it++)
+    // {
+    //     std::cout << *it << '\n';
+    // }
+    // cout << endl;
 
     // Should not overflow
-    // buffer.Push(5);
-
-    for (auto i : buffer)
-        cout << "for: " << i << "\n" << endl;
+    cout << buffer.Pop() << endl;
+    buffer.Push(5);
 
     return new SampleApp();
 }
--- Tefnout/src/Tefnout/Container/RingBuffer.hpp
+++ Tefnout/src/Tefnout/Container/RingBuffer.hpp
@@ -1,7 +1,7 @@
 #ifndef __RINGBUFFER__HPP
 #define __RINGBUFFER__HPP
 
-#include "TefnoutPCH.hpp"  // Fetch <iterator> and <array>
+#include "TefnoutPCH.hpp" // Fetch <iterator> and <array>
 
 #include "Tefnout/Core/Logger.hpp"
 
@@ -20,34 +20,211 @@ enum class Response : uint32_t
 template <typename T, std::size_t TCapacity> class Ring
 {
   public:
-    // Make refactoring easier with STL friendly alias
+    // Required alias for STL trait definition
+    using size_type = std::size_t;
     using value_type = T;
     using reference = value_type &;
-    using pointer = value_type *;
     using const_reference = const value_type &;
-    using const_pointer = const value_type *;
 
-    using size_type = std::size_t;
-    using container_type = std::array<T, TCapacity>;
+    typedef std::random_access_iterator_tag iterator_category;
 
+private:
+    // Alias for all available iterators
+    template <typename TBufferPointerType> struct RingIterator;
+
+    using iterator_type = RingIterator<Ring<T, TCapacity>>;
+    using const_iterator_type = RingIterator<const Ring<T, TCapacity>>;
+    using reverse_iterator_type = std::reverse_iterator<iterator_type>;
+    using const_reverse_iterator_type = std::reverse_iterator<const_iterator_type>;
+
+public:
     // Using 1 for initial value of head removes the need to
     // test at each insertion if buffer is empty
-    Ring() : m_head(1), m_tail(0), m_pendingSize(0)
+    Ring() : m_head(0), m_tail(0), m_pendingSize(0)
     {
     }
 
-    // template <bool isconst = false> // For const or non const iterator
+    const_reference Front() const
+    {
+        return m_container[m_head];
+    }
+
+    const_reference Back() const
+    {
+        return m_container[m_tail];
+    }
+
+    reference Front()
+    {
+        return m_container[m_head];
+    }
+
+    reference Back()
+    {
+        return m_container[m_tail];
+    }
+
+    value_type Pop()
+    {
+        auto old_head = m_head;
+        UpdateHeadAndSize();
+        return m_container[old_head];
+    }
+
+    Response TryPush(T obj)
+    {
+        auto response = Response::Unknown;
+
+        if (m_pendingSize != TCapacity)
+        {
+            m_container[m_tail] = obj;
+            UpdateTailAndSize();
+            response = Response::Ok;
+        }
+        else
+        {
+            // Do nothing
+            TEFNOUT_ERROR("Overflow of Ring occurs (TCapacity = {0})", TCapacity);
+            response = Response::Overflow;
+        }
+
+        return response;
+    }
+
+    Response Push(T obj)
+    {
+        m_container[m_tail] = obj;
+        auto response = Response::Unknown;
+
+        if (m_pendingSize != TCapacity)
+        {
+            UpdateTailAndSize();
+            response = Response::Ok;
+        }
+        else
+        {
+            // Advance both due to overflow
+            // Oldest item is lost
+            UpdateHeadAndSize();
+            UpdateTailAndSize();
+            TEFNOUT_ERROR("Overflow of Ring occurs (TCapacity = {0})", TCapacity);
+            response = Response::Overflow;
+        }
+
+        TEFNOUT_DEBUG("tail={1} head={2} (pending = {0})", m_pendingSize, m_tail, m_head);
+
+        return response;
+    }
+
+    bool IsFull()
+    {
+        return m_pendingSize == TCapacity;
+    }
+
+    bool IsEmpty()
+    {
+        return m_pendingSize == 0;
+    }
+
+    bool IsPending()
+    {
+        return !IsEmpty();
+    }
+
+    void Clear()
+    {
+        m_container.clear();
+        m_head = 0;
+        m_tail = 0;
+        m_pendingSize = 0;
+    }
+
+    // Be able to create iterator for our type
+    iterator_type begin()
+    {
+        return RingIterator(this, m_head, 0);
+    }
+    iterator_type end()
+    {
+        return RingIterator(this, m_head, m_pendingSize);
+    }
+
+    const_iterator_type cbegin() const
+    {
+        return RingIterator(this, m_head, 0);
+    }
+    const_iterator_type cend() const
+    {
+        return RingIterator(this, m_head, m_pendingSize);
+    }
+
+    reverse_iterator_type rbegin()
+    {
+        return RingIterator(this, m_tail, 0);
+    }
+    reverse_iterator_type rend()
+    {
+        return RingIterator(this, m_tail, m_pendingSize);
+    }
+
+    const_reverse_iterator_type crbegin() const
+    {
+        return RingIterator(this, m_tail, 0);
+    }
+    const_reverse_iterator_type crend() const
+    {
+        return RingIterator(this, m_tail, m_pendingSize);
+    }
+
+  private:
+    // Thin wrapper around a raw array without overhead
+    std::array<T, TCapacity> m_container;
+    size_type m_head;
+    size_type m_tail;
+    size_type m_pendingSize;
+
+    reference operator[](size_type index)
+    {
+        auto indexInBounds = index % TCapacity;
+        return m_container[indexInBounds];
+    }
+
+    const_reference &operator[](size_type index) const
+    {
+        const Ring<T, TCapacity>& itCons = *this;
+        auto indexInBounds = index % TCapacity;
+        return const_cast<reference>(itCons[indexInBounds]);
+        // return const_cast<reference>(m_container[indexInBounds]);
+    }
+
+    inline void UpdateHeadAndSize()
+    {
+        m_head = (m_head + 1) % TCapacity;
+        --m_pendingSize;
+    }
+
+    inline void UpdateTailAndSize()
+    {
+        m_tail = (m_tail + 1) % TCapacity;
+        ++m_pendingSize;
+    }
 
     /**
      * @brief      Iterator implementation for Ring as a bidirectionnal iterator.
      */
+    template <typename TBufferPointerType = Ring<T, TCapacity>>
     struct RingIterator
     {
-        // STL friendly for optimization and algorithm selection
-        using iterator_category = std::forward_iterator_tag;
-        // using difference_type = std::ptrdiff_t;
+        // STL friendly for optimization and algorithm selection using Traits
+        using iterator_category = std::bidirectional_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = T;
+        using pointer = value_type *;
+        using reference = value_type &;
+        using const_reference = const value_type &;
+        using const_pointer = const value_type *;
 
-        RingIterator(Ring<T, TCapacity> *ptr, size_type head, size_type delta)
+        RingIterator(TBufferPointerType *ptr, size_type head, size_type delta)
             : buffer_ptr(ptr), m_head(head), m_delta(delta)
         {
         }
@@ -104,34 +281,8 @@ template <typename T, std::size_t TCapacity> class Ring
             return tmp;
         }
 
-        // @MAYBE random access
-        // Needed for random access iterator
-        // Currently not needed
-        // RingIterator operator+(difference_type n)
-        // {
-        //     RingIterator tmp = *this;
-        //     tmp.m_delta += n;
-        //     return tmp;
-        // }
-        // RingIterator &operator+=(difference_type n)
-        // {
-        //     m_delta += n;
-        //     return *this;
-        // }
-        // RingIterator operator-(difference_type n)
-        // {
-        //     RingIterator tmp = *this;
-        //     tmp.m_delta -= n;
-        //     return tmp;
-        // }
-        // RingIterator &operator-=(difference_type n)
-        // {
-        //     m_delta -= n;
-        //     return *this;
-        // }
-
         // friend allows to declare this operator as non-member
-        // but still getting access to private fields in implémentation
+        // but still getting access to private fields in implementation
         friend bool operator==(const RingIterator &a, const RingIterator &b)
         {
             return (a.m_head + a.m_delta) == (b.m_head + b.m_delta);
@@ -141,121 +292,28 @@ template <typename T, std::size_t TCapacity> class Ring
             return (a.m_head + a.m_delta) != (b.m_head + b.m_delta);
         };
 
+        friend bool operator<(const RingIterator &a, const RingIterator &b)
+        {
+                return (a.m_head + a.m_pendingSize < b.m_head + b.m_pendingSize);
+        }
+        friend bool operator<=(const RingIterator &a, const RingIterator &b)
+        {
+                return (a.m_head + a.m_pendingSize <= b.m_head + b.m_pendingSize);
+        }
+        friend bool operator >(const RingIterator &a, const RingIterator &b)
+        {
+            return !a->operator<=(b);
+        }
+        friend bool operator>=(const RingIterator &a, const RingIterator &b)
+        {
+            return !a->operator<(b);
+        }
+
       private:
-        Ring<T, TCapacity> *buffer_ptr;
+        TBufferPointerType *buffer_ptr;
         size_type m_head;
         size_type m_delta;
     };
-
-    const_reference Front() const
-    {
-        return m_container[m_head];
-    }
-
-    const_reference Back() const
-    {
-        return m_container[m_tail];
-    }
-
-    reference Front()
-    {
-        return m_container[m_head];
-    }
-
-    reference Back()
-    {
-        return m_container[m_tail];
-    }
-
-    value_type Pop()
-    {
-        auto old_head = m_head;
-        UpdateHeadAndSize();
-        return m_container[old_head];
-    }
-
-    Response Push(T obj)
-    {
-        m_container[m_tail] = obj;
-        auto response = Response::Unknown;
-
-        if (m_pendingSize != TCapacity)
-        {
-            UpdateTailAndSize();
-            response = Response::Ok;
-        }
-        else
-        {
-            // Advance both due to overflow
-            // Oldest item is lost
-            UpdateHeadAndSize();
-            UpdateTailAndSize();
-            TEFNOUT_ERROR("Overflow of Ring occurs (TCapacity = {0})", TCapacity);
-            response = Response::Overflow;
-        }
-
-        TEFNOUT_DEBUG("tail={1} head={2} (pending = {0})", m_pendingSize, m_tail, m_head);
-
-        return response;
-    }
-
-    bool IsItemPending()
-    {
-        return m_head != m_tail;
-    }
-
-    bool IsEmpty()
-    {
-        return m_head == m_tail == 0;
-    }
-
-    void Clear()
-    {
-        m_container.clear();
-        m_head = 0;
-        m_tail = 0;
-    }
-
-    // Be able to create iterator for our type
-    RingIterator begin()
-    {
-        return RingIterator(this, m_head, 0);
-    }
-    RingIterator end()
-    {
-        return RingIterator(this, m_head, m_pendingSize);
-    }
-
-  private:
-    // Thin wrapper around a raw array without overhead
-    container_type m_container;
-    size_type m_head;
-    size_type m_tail;
-    size_type m_pendingSize;
-
-    T &operator[](size_type index)
-    {
-        auto indexInBounds = index % TCapacity;
-        return m_container[indexInBounds];
-    }
-
-    const T &operator[](size_type index) const
-    {
-        auto indexInBounds = index % TCapacity;
-        return m_container[indexInBounds];
-    }
-
-    inline void UpdateHeadAndSize()
-    {
-        m_head = (m_head + 1) % TCapacity;
-        --m_pendingSize;
-    }
-
-    inline void UpdateTailAndSize()
-    {
-        m_tail = (m_tail + 1) % TCapacity;
-        ++m_pendingSize;
-    }
 };
 } // namespace Buffer
 } // namespace Tefnout
