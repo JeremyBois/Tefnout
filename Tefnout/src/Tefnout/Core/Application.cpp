96f416129d4a0a3ca6b40547a429119c0274084e
JeremyBois <JeremyBoisGit@pm.me>
2021-02-01 00:15:54 +0100

    ✨ - Tefnout engine now have a window and can forward events to main application.

--- Tefnout/src/Tefnout/Core/Application.cpp
+++ Tefnout/src/Tefnout/Core/Application.cpp
@@ -1,13 +1,60 @@
 #include "Application.hpp"
+#include "Tefnout/Core/Logger.hpp"
+#include "Tefnout/Event/Event.hpp"
+#include "Tefnout/Window/IWindow.hpp"
+#include "Tefnout/Window/WindowFactory.hpp"
 
 namespace Tefnout
 {
 
+Application *Application::s_Instance = nullptr;
+
+Application::Application()
+    : m_running(false), m_pWindow(Window::Create(Window::GenericProperties{}))
+{
+    s_Instance = this;
+
+    using namespace std::placeholders;
+    m_pWindow->SetEventCallback(std::bind(&Application::OnWindowEvent, this, _1));
+
+    if (m_pWindow)
+    {
+        m_running = true;
+    }
+}
+Application::~Application()
+{
+}
+
 void Application::Run()
 {
-    while (true)
+    while (m_running)
     {
+        m_pWindow->OnUpdate();
+        m_pWindow->OnRender();
     };
+}
+
+void Application::Close()
+{
+    m_running = false;
+}
+
+void Application::OnWindowEvent(Event::IEvent &event)
+{
+    TEFNOUT_DEBUG("Get {0}", event);
+
+    Event::Information eventInformation = event.GetInformation();
+
+    switch (eventInformation.Type)
+    {
+    case Event::Kind::WindowClosed: {
+        m_running = false;
+        break;
+    }
+    default:
+        break;
+    }
 }
 
 } // namespace Tefnout
--- Tefnout/src/Tefnout/Core/Application.hpp
+++ Tefnout/src/Tefnout/Core/Application.hpp
@@ -2,6 +2,8 @@
 #define __APPLICATION__HPP
 
 #include "Platform.hpp"
+#include "Tefnout/Event/Event.hpp"
+#include "Tefnout/Window/IWindow.hpp"
 
 namespace Tefnout
 {
@@ -11,10 +13,24 @@ namespace Tefnout
 class TEFNOUT_API Application
 {
   public:
-    Application() = default;
-    virtual ~Application() = default;
+    Application();
+    virtual ~Application();
+
+    inline Window::IWindow &GetWindow()
+    {
+        return *m_pWindow;
+    }
 
     void Run();
+    void Close();
+    void OnWindowEvent(Event::IEvent &event);
+
+  private:
+
+    bool m_running;
+    std::unique_ptr<Window::IWindow> m_pWindow;
+
+    static Application *s_Instance;
 };
 
 Tefnout::Application *CreateApplication();
