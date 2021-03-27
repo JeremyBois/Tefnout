33b865d3ea23ca9736d871af658c0eccc22e57ae
JeremyBois <JeremyBoisGit@pm.me>
2021-03-27 09:38:59 +0100

    ✅ - Add tests for `Timer` class.

--- Tests/src/Utility/Timer.cpp
+++ Tests/src/Utility/Timer.cpp
@@ -1,9 +1,9 @@
 #include "catch2/catch.hpp"
 
 #include <chrono>
+#include <thread>
 
 #include "Tefnout.hpp"
-
 
 /*
  Unit-Tests for Timer implementation.
@@ -13,14 +13,21 @@
  @NOTE
 */
 
-
 TEST_CASE("Timer can be created with different tick type and different callback", "[Timer]")
 {
     SECTION("Timer using double for internal time representation")
     {
         using timeType = std::chrono::duration<double, std::milli>;
 
-        Tefnout::Utility::Timer<timeType> timer("Couscous");
+        auto test_callback = [](const Tefnout::Utility::TimerReport<timeType> report) -> void {
+            // std::cout << report.Start << "--->" << report.End << "(ID=" << report.Id << ")"
+            //           << std::endl;
+            REQUIRE(report.Start == report.End - 2);
+        };
+
+        Tefnout::Utility::Timer<timeType> timer("Couscous", test_callback);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
     }
 
     SECTION("Timer with user defined callback")
@@ -28,10 +35,11 @@ TEST_CASE("Timer can be created with different tick type and different callback"
         using timeType = std::chrono::hours;
 
         auto test_callback = [](const Tefnout::Utility::TimerReport<timeType> report) -> void {
-            std::cout << report.Start << "--->" << report.End << "(ID=" << report.Id << ")"
-                      << std::endl;
+            REQUIRE(report.Start == report.End);
         };
 
-        Tefnout::Utility::Timer<timeType> timer("Couscous");
+        Tefnout::Utility::Timer<timeType> timer("Couscous", test_callback);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
     }
 }
